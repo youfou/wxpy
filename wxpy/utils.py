cbd377dea7c6dedcc2a5ae9d0c0b5e6caf6550f6
Youfou <youfou@qq.com>
2017-02-27 03:58:17 +0800

    Add encoding comment

--- wxpy/utils.py
+++ wxpy/utils.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
 import logging
 import pprint
 import random
--- wxpy/wx.py
+++ wxpy/wx.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
 import datetime
 import inspect
 import logging
