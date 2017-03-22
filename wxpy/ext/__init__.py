437aed61dffdfe5d72359447d35701c59e3fa270
Youfou <youfou@qq.com>
2017-03-23 03:10:57 +0800

    + IBot()
    集成小 i 机器人 (由 hanx11 贡献)

--- wxpy/ext/__init__.py
+++ wxpy/ext/__init__.py
@@ -1,2 +1,5 @@
-from .tuling import Tuling
+from .ibot import IBot
 from .logging_with_wechat import WeChatLoggingHandler, get_wechat_logger
+from .tuling import Tuling
+
+
--- wxpy/utils/__init__.py
+++ wxpy/utils/__init__.py
@@ -1,4 +1,4 @@
 from .console import embed, shell_entry
-from .misc import enhance_connection, ensure_list, get_receiver, get_context_user_id, get_user_name, \
+from .misc import enhance_connection, ensure_list, get_receiver, get_user_name, \
     handle_response, match_attributes, match_name, match_text, smart_map, wrap_user_name
 from .tools import dont_raise_response_error, ensure_one, mutual_friends
