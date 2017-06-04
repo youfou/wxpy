fedce3a71e24ea74df935f57f5033bce28271f5e
youfou <youfou@qq.com>
2017-06-04 19:29:48 +0800

    fix -1001 issue
    avoid to update void shadow groups repeatedly

--- wxpy/api/chats/groups.py
+++ wxpy/api/chats/groups.py
@@ -7,15 +7,35 @@ class Groups(list):
     群聊的合集，可用于按条件搜索
     """
 
+    # 记录已知的 shadow group 和 valid group
+    # shadow group 直接抛弃
+    # valid group 直接通过
+    # 其他的需要确认是否包含机器人自身，并再分类到上面两种群中
+
+    shadow_group_user_names = list()
+    valid_group_user_names = list()
+
     def __init__(self, group_list=None):
         if group_list:
             # Web 微信服务端似乎有个 BUG，会返回不存在的群
             # 具体表现为: 名称为先前退出的群，但成员列表却完全陌生
             # 因此加一个保护逻辑: 只返回"包含自己的群"
 
-            super(Groups, self).__init__(
-                filter(lambda x: x.bot.self in x, group_list)
-            )
+            groups_to_init = list()
+
+            for group in group_list:
+                if group.user_name in Groups.shadow_group_user_names:
+                    continue
+                elif group.user_name in Groups.valid_group_user_names:
+                    groups_to_init.append(group)
+                else:
+                    if group.bot.self in group:
+                        Groups.valid_group_user_names.append(group.user_name)
+                        groups_to_init.append(group)
+                    else:
+                        Groups.shadow_group_user_names.append(group.user_name)
+
+            super(Groups, self).__init__(groups_to_init)
 
     def search(self, keywords=None, users=None, **attributes):
         """
